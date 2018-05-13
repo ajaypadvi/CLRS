6a0ef33f880b2aab6d53b617a5e5e3c3e004e8e5
Ajay Padvi <ajay.padvi@smartstream-stp.com>
2018-05-13 11:46:04 +0530

    Initial commit Chapter 1 and 2 examples and problems solved

--- src/main/java/com/clrs/chapter01/Exercise1_2_2.java
+++ src/main/java/com/clrs/chapter01/Exercise1_2_2.java
@@ -1,9 +1,10 @@
 package com.clrs.chapter01;
 
-public class Exercise123 {
+public class Exercise1_2_2 {
+
     public static void main(String[] args) {
         int n = 2;
-        while (100 * n * n > Math.pow(2, n)) {
+        while (Double.compare(Math.pow(2, (n / 8)), n) < 0) {
             n++;
         }
         System.out.println(n);
--- src/main/java/com/clrs/chapter01/Exercise1_2_3.java
+++ src/main/java/com/clrs/chapter01/Exercise1_2_3.java
@@ -1,10 +1,9 @@
 package com.clrs.chapter01;
 
-public class Exercise122 {
-
+public class Exercise1_2_3 {
     public static void main(String[] args) {
         int n = 2;
-        while (Double.compare(Math.pow(2, (n / 8)), n) < 0) {
+        while (100 * n * n > Math.pow(2, n)) {
             n++;
         }
         System.out.println(n);
