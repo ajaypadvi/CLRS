2b9aaba156698d5691386f53957e285d279afba8
Ajay Padvi <ajay.padvi@smartstream-stp.com>
2018-05-14 09:50:54 +0530

    Calculating inversions in array using brute force

---
+++ src/main/java/com/clrs/chapter02/Problem_2_4.java
@@ -0,0 +1,33 @@
+package com.clrs.chapter02;
+
+public class Problem_2_4 {
+    /**
+     * Solution which finds inversion pairs in o(n2)
+     *
+     * @param a
+     */
+    public static void inversionPairs(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    System.out.println("[" + i + "," + j + "]");
+                }
+            }
+        }
+    }
+
+    /**
+     * An improved solution which finds inversion pairs in o(n2)
+     *
+     * @param a
+     */
+    public static void inversionPairsImproved(int[] a) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2, 3, 8, 6, 1};
+        inversionPairs(a);
+
+    }
+}
