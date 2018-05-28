f744f42a8b980d262fe98c700039ba8442b4d3a3
Ajay Padvi <ajay.padvi@smartstream-stp.com>
2018-05-28 12:40:06 +0530

    Implemented Dequeue operations

--- src/main/java/com/clrs/chapter10/Dequeue.java
+++ src/main/java/com/clrs/chapter10/Dequeue.java
@@ -1,8 +1,95 @@
 package com.clrs.chapter10;
 
+import java.util.Arrays;
+
 /**
  * Doubly ended queue
  * Exercise 10.1-5
  */
 public class Dequeue {
+
+    private int head;
+    private int tail;
+    private int[] a;
+    private int elements;// This extra attribute is to make sure that queue wont overflow or underflow
+
+    public Dequeue(int size) {
+        this.a = new int[size];
+        this.head = 0;
+        this.tail = 0;
+        this.elements = 0;
+    }
+
+    public void leftEnqueue(int x) {
+        if (elements == a.length) {
+            throw new DequeueOverflowException();
+        }
+        if (head == 0) {
+            head = a.length - 1;//wraparound
+        } else {
+            head = head - 1;
+        }
+        a[head] = x;
+        elements++;
+        System.out.println("LEFT ENQUEUE : " + toString());
+    }
+
+    public void rightEnqueue(int x) {
+        if (elements == a.length) {
+            throw new Queue.QueueOverflowException();
+        }
+        a[tail] = x;
+        if (tail == a.length - 1) {
+            tail = 0;//wraparound
+        } else {
+            tail = tail + 1;
+        }
+        elements++;
+        System.out.println("RIGHT ENQUEUE : " + toString());
+    }
+
+    public int leftDequeue() {
+        if (elements == 0) {
+            throw new Queue.QueueUnderflowException();
+        }
+        int x = a[head];
+        if (head == a.length - 1) {
+            head = 0; //wraparound
+        } else {
+            head = head + 1;
+        }
+        elements--;
+        System.out.println("LEFT DEQUEUE : " + toString());
+        return x;
+    }
+
+    public int rightDequeue() {
+        if (elements == 0) {
+            throw new DequeueUnderflowException();
+        }
+        int x = a[tail];
+        if (tail == 0) {
+            tail = a.length - 1;// wraparound
+        } else {
+            tail = tail - 1;
+        }
+        return x;
+    }
+
+    public static final class DequeueOverflowException extends RuntimeException {
+        DequeueOverflowException() {
+            super("dequeue overflow");
+        }
+    }
+
+    public static final class DequeueUnderflowException extends RuntimeException {
+        DequeueUnderflowException() {
+            super("dequeue underflow");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "#HEAD=" + head + " #TAIL=" + tail + " #ARRAY= " + Arrays.toString(a);
+    }
 }
--- src/test/java/com/clrs/chapter10/DequeueTest.java
+++ src/test/java/com/clrs/chapter10/DequeueTest.java
@@ -1,4 +1,48 @@
 package com.clrs.chapter10;
 
+import org.junit.Test;
+
 public class DequeueTest {
+
+    @Test
+    public void testDequeue() {
+        Dequeue dequeue = new Dequeue(10);
+        dequeue.rightEnqueue(10);
+        dequeue.rightEnqueue(110);
+        dequeue.rightEnqueue(30);
+        dequeue.rightEnqueue(40);
+        dequeue.rightEnqueue(50);
+
+        dequeue.leftEnqueue(60);
+        dequeue.leftEnqueue(70);
+        dequeue.leftEnqueue(80);
+        dequeue.leftEnqueue(90);
+        dequeue.leftEnqueue(100);
+    }
+
+    @Test(expected = Dequeue.DequeueOverflowException.class)
+    public void testDequeueOverflow() {
+        Dequeue dequeue = new Dequeue(10);
+        dequeue.rightEnqueue(10);
+        dequeue.rightEnqueue(20);
+        dequeue.rightEnqueue(30);
+        dequeue.rightEnqueue(40);
+        dequeue.rightEnqueue(50);
+
+        dequeue.leftEnqueue(60);
+        dequeue.leftEnqueue(70);
+        dequeue.leftEnqueue(220);
+        dequeue.leftEnqueue(90);
+        dequeue.leftEnqueue(100);
+
+
+        //Below statement will cause dequeue overflow excpetion
+        dequeue.leftEnqueue(44);
+    }
+
+    @Test(expected = Dequeue.DequeueUnderflowException.class)
+    public void testDequeueUnderflow() {
+        Dequeue dequeue = new Dequeue(10);
+        dequeue.rightDequeue();
+    }
 }
